f336aec585ac92506a7033c1825db4f2903f1875
Junk-debug <maks.naumovich@gmail.com>
2024-12-05 12:55:15 +0100

    feat: add framer-motion, update landing design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,47 @@
+'use client';
+
 import { buttonVariants } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 import Link from 'next/link';
+import { motion } from 'framer-motion';
 
 export default function HomePage() {
   return (
-    <main className="w-full h-dvh flex items-center justify-center bg-gradient-to-r from-white via-gray-50 to-white dark:from-gray-900 dark:via-gray-800 dark:to-gray-900">
-      <div className="container px-4 md:px-6 py-12 space-y-4 text-center">
-        <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
-          Master Your Tasks, Conquer Your Day
-        </h1>
-        <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
-          Our Todo app helps you organize, prioritize, and accomplish your tasks
-          with ease. Stay productive and achieve your goals.
-        </p>
-
-        <Link className={cn(buttonVariants({ size: 'lg' }))} href="/home">
-          Get started
-        </Link>
+    <main className="w-full h-dvh flex items-center justify-center bg-gradient-to-b from-card to-rose-100  dark:to-rose-950">
+      <div className="absolute inset-0 bg-grid" />
+      <div className="z-50 container px-6 md:px-8 py-16 space-y-6 text-center">
+        <motion.div
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <h1 className="text-primary text-4xl font-extrabold tracking-tight sm:text-5xl md:text-6xl lg:text-7xl">
+            Just todo it
+          </h1>
+        </motion.div>
+        <motion.p
+          className="mx-auto max-w-[700px] md:text-xl"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+        >
+          Your todo list is a mess, we know. That&apos;s why we&apos;re here to
+          help you tame the beast and get your life back on track. Or at least,
+          you know, make you feel better about procrastinating.
+        </motion.p>
+        <motion.div
+          className="flex flex-col items-center gap-1"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.4 }}
+        >
+          <Link className={cn(buttonVariants({ size: 'lg' }))} href="/home">
+            Get your life together
+          </Link>
+          <span className="text-xs text-muted-foreground">
+            (or at least pretend to)
+          </span>
+        </motion.div>
       </div>
     </main>
   );
