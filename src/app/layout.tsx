9f14aa49611fcd1ed72cb23ac4118f3c0de6c7f5
Junk-debug <maks.naumovich@gmail.com>
2024-12-06 12:01:47 +0100

    feat: update manifest, refactor meta tags in head to metadata anb viewport objects

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
 import { SessionProvider } from 'next-auth/react';
@@ -17,9 +17,28 @@ const geistMono = localFont({
   weight: '100 900',
 });
 
+export const viewport: Viewport = {
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: '#fff' },
+    { media: '(prefers-color-scheme: dark)', color: '#0C0A09' },
+  ],
+};
+
 export const metadata: Metadata = {
   title: 'Just todo it',
   description: 'Web todo application',
+  manifest: '/manifest.webmanifest',
+  appleWebApp: {
+    title: 'Just todo it',
+  },
+  icons: {
+    icon: [
+      { url: '/favicon-96x96.png', type: 'image/png', sizes: '96x96' },
+      { url: '/favicon.svg', type: 'image/svg+xml' },
+    ],
+    shortcut: '/favicon.ico',
+    apple: { url: '/apple-touch-icon.png', sizes: '180x180' },
+  },
 };
 
 export default function RootLayout({
@@ -31,23 +50,6 @@ export default function RootLayout({
     <QueryClientProvider>
       <SessionProvider>
         <html lang="en">
-          <head>
-            <link
-              rel="icon"
-              type="image/png"
-              href="/favicon-96x96.png"
-              sizes="96x96"
-            />
-            <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-            <link rel="shortcut icon" href="/favicon.ico" />
-            <link
-              rel="apple-touch-icon"
-              sizes="180x180"
-              href="/apple-touch-icon.png"
-            />
-            <meta name="apple-mobile-web-app-title" content="Just todo it" />
-            <link rel="manifest" href="/site.webmanifest" />
-          </head>
           <body
             className={`${geistSans.variable} ${geistMono.variable} font-geist antialiased`}
           >
